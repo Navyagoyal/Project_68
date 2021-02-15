f35b7d729923e000cc037c024f613095a4b2b1b4
Navyagoyal <navyagoyalcool@gmail.com>
2021-02-15 14:08:56 +0530

    Add files

--- App.js
+++ App.js
@@ -1,15 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {createBottomTabNavigator} from 'react-native-tabs';
+import {createAppContainer} from 'react-navigation';
+import facebook from './screens/facebook';
+import instagram from './screens/instagram';
 
-export default function App() {
+export default class App extends React.Component{
+  render(){
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <AppContainer/>
     </View>
   );
 }
+}
+const TabNavigator=createBottomTabNavigator({
+  facebook:{screens:facebook},
+  instagram:{screens:instagram}
+},
+{
+  defaultNavigationOptions:({navigation})=>({
+    tabBarIcon:({})=>{
+      const routeName=navigation.state.routeName;
+      if(routeName === "facebook")
+      {
+        return(
+          <Image source={require("./assets/facebook.jpg")} style={{width:40,height:40}}/>
+        );
+      }
+      else if(routeName === "instagram")
+      {
+        return(
+          <Image source={require("./assets/instagram.jpeg")} style={{width:40,height:40}}/>
+        )
+      }
+    }
+  })
+}
+)
+const AppContainer=createAppContainer(TabNavigator);
 
 const styles = StyleSheet.create({
   container: {
